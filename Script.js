23ee22f244bd159af28dac452e38da8e4ae12c5b
CELIO LAZARO PEREIRA <celio.lpereira@senacsp.edu.br>
2024-09-25 15:07:52 -0300

    first commit

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 // Solicitar ao aluno que insira a nota
-let idade = parseInt (prompt("Digite a sua nota"));
+let idade = parseFloat (prompt("Digite a sua nota"));
 // Se a idade for maior que 18, o usuário poderá acessar o sistema
 if (idade >=7){
     alert ("Aluno aprovado!😉");
